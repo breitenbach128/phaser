477fa9cda5a2968b44e9672ac14099d9238c0ffa
Ben Breitenbach <breitenbach128@hotmail.com>
2019-06-03 23:08:40 -0500

    expanding template, starting MP base

--- public/README.md
+++ public/README.md
@@ -1,2 +1,4 @@
-Initial Code for a New Phaser game as I learn it.
+https://phasertutorials.com/creating-a-simple-multiplayer-game-in-phaser-3-with-an-authoritative-server-part-2/
+Using this current tutorial on expanding my socket IO.
 
+Lots to do. Need to craft the multiplayer from the ground up.
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var server = require('http').Server(app);1
 var io = require('socket.io').listen(server);
 
+var players = {};
+
 app.use(express.static(__dirname + '/public'));
  
 app.get('/', function (req, res) {
@@ -11,8 +13,25 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
     console.log('a user connected');
+    // create a new player and add it to our players object
+    players[socket.id] = {
+      rotation: 0,
+      x: Math.floor(Math.random() * 700) + 50,
+      y: Math.floor(Math.random() * 500) + 50,
+      playerId: socket.id,
+      team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+    };
+    // send the players object to the new player
+    socket.emit('currentPlayers', players);
+    // update all other players of the new player
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+
     socket.on('disconnect', function () {
-      console.log('user disconnected');
+      console.log('user disconnected');  
+      // remove this player from our players object
+      delete players[socket.id];
+      // emit a message to all players to remove this player
+      io.emit('disconnect', socket.id);
     });
 });
 
