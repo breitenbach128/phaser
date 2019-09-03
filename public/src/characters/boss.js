b938731fc808307b37e6e3fb5ddaa512b386f504
Ben Breitenbach <breitenbach128@hotmail.com>
2019-09-03 00:02:43 -0500

    Boss-Spider movement, interaction, design

--- public/src/characters/boss.js
+++ public/src/characters/boss.js
@@ -7,12 +7,12 @@ class Boss extends Phaser.Physics.Matter.Sprite{
         const { Body, Bodies } = Phaser.Physics.Matter.Matter; // Native Matter modules
         const { width: w, height: h } = this;
         //Set Control Sensor - Player can't collide with mirrors, but bullets can. Sensor can detect player inputs.
-        const coreArea =  Bodies.rectangle(0, 0, this.width*.5, this.height*.35, { isSensor: false });
+        const coreArea =  Bodies.rectangle(0, 0, this.width*.5, this.height*.35, { chamfer: {radius: 5}, isSensor: false });
         this.sensors = {
-            bottom: Bodies.rectangle(0, h * 0.18, w * 0.28, 2, { isSensor: true }),
-            top: Bodies.rectangle(0, -h * 0.18, w * 0.28, 2, { isSensor: true }),
-            left: Bodies.rectangle(-w * 0.25, 0, 2, h * 0.22, { isSensor: true }),
-            right: Bodies.rectangle(w * 0.25, 0, 2, h * 0.22, { isSensor: true })
+            bottom: Bodies.rectangle(0, h * 0.18, w * 0.40, 2, { isSensor: true }),
+            top: Bodies.rectangle(0, -h * 0.18, w * 0.40, 2, { isSensor: true }),
+            left: Bodies.rectangle(-w * 0.25, 0, 2, h * 0.30, { isSensor: true }),
+            right: Bodies.rectangle(w * 0.25, 0, 2, h * 0.30, { isSensor: true })
           };
         this.sensors.bottom.label = "SPIDER_BOTTOM";
         this.sensors.top.label = "SPIDER_TOP";
@@ -34,10 +34,12 @@ class Boss extends Phaser.Physics.Matter.Sprite{
         this
         .setExistingBody(mainBody)
         .setFixedRotation() 
-        .setIgnoreGravity(false);  
+        .setIgnoreGravity(true);  
 
         //Custom Props
         this.touching = {up:0,down:0,left:0,right:0};
+        this.mv_speed = 1;
+        this.fall_speed = 6;
         //Collision
         this.scene.matter.world.on('beforeupdate', function (event) {
             this.touching.left = 0;
@@ -95,11 +97,61 @@ class Boss extends Phaser.Physics.Matter.Sprite{
         
         this.debug = this.scene.add.text(this.x, this.y-16, 'bright', { resolution: 2,fontSize: '10px', fill: '#00FF00' });
         
+        //AI
+        this.wanderDirection = 1;//Clockwise
+        this.falltime = 0;
+        this.attackDelay = this.scene.time.addEvent({ delay: 10, callback: this.startAttack, callbackScope: this, loop: false });
+        this.jumping = false;
     }
     update(time, delta)
     {       
         this.anims.play('boss-spider', true);
         this.debug.setPosition(this.x, this.y-64);
         this.debug.setText("TOUCH: L:"+String(this.touching.left)+" R:"+String(this.touching.right)+" U:"+String(this.touching.up)+" D:"+String(this.touching.down));
+
+        //Check for Player to attack
+        let disToSolana = Phaser.Math.Distance.Between(this.x,this.y,solana.x,solana.y);
+        if(disToSolana < 256){
+            let rayToSolana = Phaser.Physics.Matter.Matter.Query.ray(this.scene.matter.world.localWorld.bodies,{x:this.x,y:this.y},{x:solana.x,y:solana.y});
+            if(rayToSolana.length < 3){
+                //LOS Not Blocked
+                //this.setIgnoreGravity(true); 
+                
+                //Check Ground, or Ceiling and jump accordingly.
+                //Move left/right and then apply up or down. The falltime will take over and cause the fall after time.
+            }
+        }
+        //ON PLATFORM BEHAVIOR
+        if(this.touching.left == 0 &&  this.touching.right == 0 && this.touching.up == 0 && this.touching.down == 0){
+            //Airborne
+            this.falltime++;
+        }else{
+            this.falltime = 0;
+            //Touching Single Direction
+            if(this.touching.left > 0 && this.touching.right == 0 && this.touching.up == 0 && this.touching.down == 0){
+                this.setVelocityY(this.mv_speed*this.wanderDirection);
+                this.setVelocityX(this.mv_speed*this.wanderDirection*-1);
+            }else if(this.touching.left == 0 && this.touching.right > 0 && this.touching.up == 0 && this.touching.down == 0){
+                this.setVelocityY(this.mv_speed*this.wanderDirection*-1);
+                this.setVelocityX(this.mv_speed*this.wanderDirection);
+            }
+
+            if(this.touching.left == 0 && this.touching.right == 0 && this.touching.up > 0 && this.touching.down == 0){
+                this.setVelocityX(this.mv_speed*this.wanderDirection*-1);
+                this.setVelocityY(this.mv_speed*this.wanderDirection*-1);
+            }else if(this.touching.left == 0 && this.touching.right == 0 && this.touching.up == 0 && this.touching.down > 0){
+                this.setVelocityX(this.mv_speed*this.wanderDirection);
+                this.setVelocityY(this.mv_speed*this.wanderDirection);
+            }
+            
+        }
+
+        //Set fall time
+        if(this.falltime > 15){
+            this.setVelocityY(this.fall_speed);
+        }
+    }
+    startAttack(){
+
     }
 }
