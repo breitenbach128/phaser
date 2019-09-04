bd402ce4627f17f07abe961d0697da407fdec6ca
Ben Breitenbach <breitenbach128@hotmail.com>
2019-09-03 23:52:11 -0500

    Boss-Spider movement, interaction, design

--- public/src/characters/boss.js
+++ public/src/characters/boss.js
@@ -11,8 +11,8 @@ class Boss extends Phaser.Physics.Matter.Sprite{
         this.sensors = {
             bottom: Bodies.rectangle(0, h * 0.18, w * 0.40, 2, { isSensor: true }),
             top: Bodies.rectangle(0, -h * 0.18, w * 0.40, 2, { isSensor: true }),
-            left: Bodies.rectangle(-w * 0.25, 0, 2, h * 0.30, { isSensor: true }),
-            right: Bodies.rectangle(w * 0.25, 0, 2, h * 0.30, { isSensor: true })
+            left: Bodies.rectangle(-w * 0.25, 0, 2, h * 0.20, { isSensor: true }),
+            right: Bodies.rectangle(w * 0.25, 0, 2, h * 0.20, { isSensor: true })
           };
         this.sensors.bottom.label = "SPIDER_BOTTOM";
         this.sensors.top.label = "SPIDER_TOP";
@@ -40,6 +40,7 @@ class Boss extends Phaser.Physics.Matter.Sprite{
         this.touching = {up:0,down:0,left:0,right:0};
         this.mv_speed = 1;
         this.fall_speed = 6;
+        this.jump_speed = 2;
         //Collision
         this.scene.matter.world.on('beforeupdate', function (event) {
             this.touching.left = 0;
@@ -95,37 +96,35 @@ class Boss extends Phaser.Physics.Matter.Sprite{
 
         //DEBUG
         
-        this.debug = this.scene.add.text(this.x, this.y-16, 'bright', { resolution: 2,fontSize: '10px', fill: '#00FF00' });
+        this.debug = this.scene.add.text(this.x, this.y-16, 'bright', { resolution: 2,fontSize: '8px', fill: '#00FF00' });
         
         //AI
         this.wanderDirection = 1;//Clockwise
         this.falltime = 0;
-        this.attackDelay = this.scene.time.addEvent({ delay: 10, callback: this.startAttack, callbackScope: this, loop: false });
+        this.attackDelay = this.scene.time.addEvent({ delay: 3000, callback: this.startAttack, callbackScope: this, loop: true });
         this.jumping = false;
     }
     update(time, delta)
     {       
         this.anims.play('boss-spider', true);
         this.debug.setPosition(this.x, this.y-64);
-        this.debug.setText("TOUCH: L:"+String(this.touching.left)+" R:"+String(this.touching.right)+" U:"+String(this.touching.up)+" D:"+String(this.touching.down));
+        this.debug.setText("L:"+String(this.touching.left)+" R:"+String(this.touching.right)+" U:"+String(this.touching.up)+" D:"+String(this.touching.down)
+        +"\n wd:"+String(this.wanderDirection));
 
         //Check for Player to attack
         let disToSolana = Phaser.Math.Distance.Between(this.x,this.y,solana.x,solana.y);
         if(disToSolana < 256){
-            let rayToSolana = Phaser.Physics.Matter.Matter.Query.ray(this.scene.matter.world.localWorld.bodies,{x:this.x,y:this.y},{x:solana.x,y:solana.y});
-            if(rayToSolana.length < 3){
-                //LOS Not Blocked
-                //this.setIgnoreGravity(true); 
-                
-                //Check Ground, or Ceiling and jump accordingly.
-                //Move left/right and then apply up or down. The falltime will take over and cause the fall after time.
-            }
+           
         }
         //ON PLATFORM BEHAVIOR
         if(this.touching.left == 0 &&  this.touching.right == 0 && this.touching.up == 0 && this.touching.down == 0){
             //Airborne
             this.falltime++;
         }else{
+            if(this.jumping){
+                this.jumping = false;
+                this.setIgnoreGravity(true);
+            }
             this.falltime = 0;
             //Touching Single Direction
             if(this.touching.left > 0 && this.touching.right == 0 && this.touching.up == 0 && this.touching.down == 0){
@@ -138,20 +137,65 @@ class Boss extends Phaser.Physics.Matter.Sprite{
 
             if(this.touching.left == 0 && this.touching.right == 0 && this.touching.up > 0 && this.touching.down == 0){
                 this.setVelocityX(this.mv_speed*this.wanderDirection*-1);
-                this.setVelocityY(this.mv_speed*this.wanderDirection*-1);
+                //this.setVelocityY(this.mv_speed*this.wanderDirection*-1);
             }else if(this.touching.left == 0 && this.touching.right == 0 && this.touching.up == 0 && this.touching.down > 0){
                 this.setVelocityX(this.mv_speed*this.wanderDirection);
-                this.setVelocityY(this.mv_speed*this.wanderDirection);
+                //this.setVelocityY(this.mv_speed*this.wanderDirection);
+            }
+            //Touching corner on ground or ceiling
+            if(this.touching.left > 0 && (this.touching.down > 0 || this.touching.up > 0)){
+                this.wanderDirection = 1;
+                console.log("T-L: Change to 1");
+            }else if(this.touching.right > 0 && (this.touching.down > 0 || this.touching.up > 0)){
+                this.wanderDirection = -1;
+                console.log("T-R: Change to -1");
             }
             
         }
 
         //Set fall time
         if(this.falltime > 15){
-            this.setVelocityY(this.fall_speed);
+            this.setIgnoreGravity(false); 
+            //this.setVelocityY(this.fall_speed);
         }
     }
+    climbToTile(){
+        //If a tile is directly above the spider, they will stop, and spit a thread of silk at the platform and climb up to it to begin patrolling again.
+        //If the player is above them, but not within LOS, they will do this as well.
+    }
     startAttack(){
-
+        if(!this.jumping){
+            this.jumping = true;
+            if(this.touching.up > 0){
+                this.setVelocityY(this.jump_speed);
+            }else{
+                this.setVelocityY(-this.jump_speed);
+            }
+            if(this.canSee(solana)){
+                if(solana.x < this.x){
+                    this.wanderDirection = -1;
+                    this.setVelocityX(-this.jump_speed*2);
+                }else{
+                    this.wanderDirection = 1;
+                    this.setVelocityX(this.jump_speed*2);
+                }
+            }else{
+                this.setVelocityX(this.jump_speed*this.wanderDirection);
+            }
+        }
+    }
+    canSee(target){
+        let rayTo = Phaser.Physics.Matter.Matter.Query.ray(this.scene.matter.world.localWorld.bodies,{x:this.x,y:this.y},{x:target.x,y:target.y});
+        if(rayTo.length < 3){
+            //LOS Not Blocked
+            //this.setIgnoreGravity(true); 
+            
+            //Check Ground, or Ceiling and jump accordingly.
+            //Move left/right and then apply up or down. The falltime will take over and cause the fall after time.
+            return true;
+        }else{
+            return false;
+        }
+        
     }
 }
